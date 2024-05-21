8ad883117cda918c4ac698a49d9109f0310117a9
acsouza2398 <63055644+acsouza2398@users.noreply.github.com>
2024-05-21 09:09:19 -0300

    feat: Refactor image handling and classification in landing page

--- landing-page/pages/classifier.py
+++ landing-page/pages/classifier.py
@@ -1,34 +1,15 @@
 import streamlit as st
-import boto3
-from dotenv import load_dotenv
-import os
 from PIL import Image
-# remove for deploy
 import sys
 sys.path.append('..')
 from model.predict import handler
 
 legend = {"jack_frost": "Jack Frost", "pixie": "Pixie", "decarabia": "Decarabia"}   
-images_dict = {"Jack Frost": "../img/Jack_Frost_sprite_small.png", "Pixie": "../img/Pixie_sprite.png", "Decarabia": "../img/Decarabia_sprite.png"}
+images_dict = {"Jack Frost": "img/Jack_Frost_sprite_small.png", "Pixie": "img/Pixie_sprite.png", "Decarabia": "img/Decarabia_sprite.png"}
 
 class Classifier():
     def __init__(self):
-        load_dotenv()
-        self.s3 = None
-        self.bucket = None
-        self.env = os.getenv("env")
-
-    def load_s3(self):
-        self.s3 = boto3.client('s3',
-                               region_name=os.getenv('AWS_REGION'),
-                               aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                               aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                               aws_session_token=os.getenv('AWS_SESSION_TOKEN'))
-        
-        self.bucket = os.getenv("AWS_BUCKET_NAME")
-        self.env = None
-
-        return
+        pass
 
     def run(self):
         st.title('Classificador de Demônios de Persona e Shin Megami Tensei')
@@ -54,21 +35,14 @@ class Classifier():
                                 img = Image.open(image[i])
                                 new_image = img.resize((150, 100))
                                 st.image(new_image)
-                                if self.env != "LOCAL":
-                                    self.load_s3()
-                                    self.s3.put_object(Key=f"{image[i].name}", Body=image[i].read(), Bucket=os.getenv('AWS_BUCKET_NAME'))
-                                    # trigger lambda with event = {"path": f"{image[i].name}"}
-                                    response = {"Jack Frost": 0.9, "Pixie": 0.05, "Decarabia": 0.05} # replace with lambda response
-                                    st.write(response)
-                                else:
-                                    response = handler({"path": image[i]}, None)
-                                    result = response["body"]
-                                    response = {legend[k]: v for k, v in result.items()}
-                                    response = sorted(response.items(), key=lambda x: x[1], reverse=True)
-                                    for k, v in response:
-                                        sprite = images_dict[k]
-                                        st.markdown(f"![{k}]({sprite}) {v*100:.2f}%", unsafe_allow_html=True)
-                                        #st.write(f"{k}: {v*100:.2f}%")
+                                response = handler({"path": image[i]}, None)
+                                result = response["body"]
+                                response = {legend[k]: v for k, v in result.items()}
+                                response = sorted(response.items(), key=lambda x: x[1], reverse=True)
+                                for k, v in response:
+                                    sprite = images_dict[k]
+                                    st.markdown(f"![{k}]({sprite}) {v*100:.2f}%", unsafe_allow_html=True)
+                                    #st.write(f"{k}: {v*100:.2f}%")
 
                 else:
                     st.write('Por favor, faça o upload de uma imagem antes de clicar em "Classificar"')
